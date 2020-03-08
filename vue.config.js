4eacf9a43a2c94df1d54262001fa887c49582edd
pbeicn <pbeicn@qq.com>
2020-03-08 17:03:43 +0800

    提交修改

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,6 @@
 /* eslint-disable prettier/prettier */
 module.exports = {
   devServer: {
-    proxy: {
-      "/sisp-business": {
-        target: "http://211.143.59.146:9045",
-        ws: true,
-        changeOrigin: true
-      },
       "/api": {
         target: "http://localhost:9000",
         ws: true,
