3f91996beb1693fb4dbc5edc32bc89f95d929e1a
pbeicn <pbeicn@qq.com>
2020-03-08 17:04:15 +0800

    提交修改

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,10 @@
 /* eslint-disable prettier/prettier */
 module.exports = {
   devServer: {
-      "/api": {
-        target: "http://localhost:9000",
-        ws: true,
-        changeOrigin: true
-      }
+    "/api": {
+      target: "http://localhost:9000",
+      ws: true,
+      changeOrigin: true
     }
   }
 };
