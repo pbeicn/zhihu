f88a1fe5305eb879f2fb504422adfa738a05e846
pbeicn <pbeicn@qq.com>
2020-03-18 17:15:18 +0800

    底部自定义图标

--- src/components/footer/foot.vue
+++ src/components/footer/foot.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
     <van-tabbar v-model="active" @change="onChange">
-      <van-tabbar-item icon="home-o">接单赚钱</van-tabbar-item>
-      <van-tabbar-item icon="search">点赞推广</van-tabbar-item>
-      <van-tabbar-item icon="friends-o">反对压制</van-tabbar-item>
-      <van-tabbar-item icon="setting-o">定制推广</van-tabbar-item>
-      <van-tabbar-item icon="setting-o">我的</van-tabbar-item>
+      <van-tabbar-item icon="home-o"
+        >接单赚钱<img slot="icon" src="@/assets/p1.png" />
+      </van-tabbar-item>
+      <van-tabbar-item icon="search"
+        >点赞推广<img slot="icon" src="@/assets/p2.png"
+      /></van-tabbar-item>
+      <van-tabbar-item icon="friends-o"
+        >反对压制<img slot="icon" src="@/assets/p3.png"
+      /></van-tabbar-item>
+      <van-tabbar-item icon="setting-o"
+        >定制推广<img slot="icon" src="@/assets/p4.png"
+      /></van-tabbar-item>
+      <van-tabbar-item icon="setting-o"
+        >我的<img slot="icon" src="@/assets/p5.png"
+      /></van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
